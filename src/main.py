d7b1997e1841df440a0b0be4f3c9984d36169146
brian618 <brianc-@live.ca>
2016-11-23 11:16:20 -0800

    rename background sub and remove extraneous calls

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from __future__ import print_function
 import cv2
 import numpy as np
 
-import backgnd_sub
+import backgroundSub
 import colorSampleLocation
 import colorSampler as cs
-import angleDerivation as ad
+from angleDerivation import AngleDerivation as ad
 from utils import image_utils
 
 class Lympht:
@@ -14,7 +14,7 @@ class Lympht:
         self.capture = cv2.VideoCapture(0)
         self.main_window_name = "lympht"
         self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.bg_sub = backgnd_sub.BackgroundSubtractor()
+        self.bg_sub = backgroundSub.BackgroundSubtractor()
         self.cs = None
         self.csl = colorSampleLocation.ColorSampleLocation(self.capture.read()[1])
         self.cs_locations = self.csl.get_color_sample_locations()
@@ -80,17 +80,16 @@ class Lympht:
                     cv2.putText(draw_frame, "hull area " + str(hull_area) + "px",
                                 (0, frame_height / 7), self.font, 0.5, (50, 50, 255), 2)
 
-                    rows, cols = thresh.shape[:2]
                     [vx, vy, x, y] = cv2.fitLine(largest_contour, cv2.DIST_L2, 0, 0.01, 0.01)
                     lefty = int((-x * vy / vx) + y)
-                    righty = int(((cols - x) * vy / vx) + y)
+                    righty = int(((frame_width - x) * vy / vx) + y)
                     
-                    verticalLine = [(cols / 2, 0), (cols / 2, rows - 1)]
-                    contourLine = [(cols - 1, righty), (0, lefty)]
+                    verticalLine = [(frame_width / 2, 0), (frame_width / 2, frame_height - 1)]
+                    contourLine = [(frame_width - 1, righty), (0, lefty)]
 
                     verticalVector = (0, 1)
                     contourVector = (vx, vy)
-                    angle = ad.AngleDerivation.findAngle(verticalVector, contourVector)
+                    angle = ad.findAngle(verticalVector, contourVector)
                     
                     cv2.line(draw_frame, contourLine[0], contourLine[1], (0, 255, 0), 2)
                     cv2.line(draw_frame, verticalLine[0], verticalLine[1], (0, 255, 0), 2)
